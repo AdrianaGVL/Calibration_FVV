affa0d0b8e2af95a9905a25028c75eaa5e3563d9
Adriana Galán Villamarín <agv@gti.ssr.upm.es>
2024-05-27 10:54:00 +0200

    scales. update

--- Error_study/scale.py
+++ Error_study/scale.py
@@ -14,7 +14,7 @@ import os
 import numpy as np
 
 # Config file
-with open('./config_file.yml', 'r') as config_file:
+with open('../config_file.yml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 config_file.close
 
@@ -28,7 +28,7 @@ reconstruction_known = f'{output_path}/{config["known_poses"]}'
 results_path = f'{output_path}/{config["scale_data"]}'
 os.makedirs(results_path, exist_ok=True)
 # App files
-jsons_path = './jsons_structures.json'
+jsons_path = '../jsons_structures.json'
 sfm_data_file = f'{reconstruction_known}/{config["known_sfm_data"]}'
 scales_data = f'{results_path}/{config["scales_file"]}'
 
@@ -58,11 +58,17 @@ f.close
 
 points_3D = []
 num_points = len(sfm_data["structure"])
-for coords in sfm_data["structure"]:
-    x_point = coords["value"]["X"][0]
-    y_point = coords["value"]["X"][1]
-    point_3D = (x_point, y_point)
-    points_3D.append(point_3D)
+key = 0
+while len(points_3D) != num_points:
+    for coords in sfm_data["structure"]:
+        if key == coords["key"]:
+            x_point = coords["value"]["X"][0]
+            y_point = coords["value"]["X"][1]
+            point_3D = (x_point, y_point)
+            points_3D.append(point_3D)
+            key +=1
+        else:
+            continue
 
 scales = []
 scales_x = []
@@ -91,6 +97,7 @@ for i in range(len(points_3D)):
         scale_ax["Y axis data"]["Adjacent point"] = i+nCorners_x
         dist_y = points_3D[i][1] - points_3D[i+nCorners_x][1]
         scale_y = abs(dist/dist_y) # Scale in the corresponding (dist) units
+        scale_ax["Y axis data"]["Scale"] = scale_y
         scales.append(scale_y)
         scales_y.append(scale_y)
     else:
@@ -101,10 +108,10 @@ for i in range(len(points_3D)):
 
     scale["Measures per point"].append(copy.deepcopy(scale_ax))
 
-scale["Measures"]["Scaling Factor"]["Mean"] = statistics.mean(scales)
-scale["Measures"]["Scaling Factor"]["Standard deviation"] = statistics.stdev(scales)
-scale["Measures"]["Scaling Factor"]["Max. value"] = max(scales)
-scale["Measures"]["Scaling Factor"]["Min. value"] = min(scales)
+scale["Scaling Factor"]["Mean"] = statistics.mean(scales)
+scale["Scaling Factor"]["Standard deviation"] = statistics.stdev(scales)
+scale["Scaling Factor"]["Max. value"] = max(scales)
+scale["Scaling Factor"]["Min. value"] = min(scales)
 
 with open(scales_data, 'w') as pqs:
     json.dump(scale, pqs, indent=4)
@@ -112,7 +119,7 @@ pqs.close
 
 # Plot Scale info
 sns.histplot(scales, kde=True, color='blue')
-hist, bin_edges = np.histogram(scales, bins='auto')
+# hist, bin_edges = np.histogram(scales, bins='auto')
 # print(bin_edges)
 plt.title('Scales distribution')
 plt.xlabel('Scale value')
@@ -122,7 +129,7 @@ plt.close()
 
 # Plot Error in x info
 sns.histplot(scales_x, kde=True, color='blue')
-hist, bin_edges = np.histogram(scales_x, bins='auto')
+# hist, bin_edges = np.histogram(scales_x, bins='auto')
 # print(bin_edges)
 plt.title('Scales distribution in the X-axis')
 plt.xlabel('Scale value')
@@ -132,7 +139,7 @@ plt.close()
 
 # Plot Error in y info
 sns.histplot(scales_y, kde=True, color='blue')
-hist, bin_edges = np.histogram(scales_y, bins='auto')
+# hist, bin_edges = np.histogram(scales_y, bins='auto')
 # print(bin_edges)
 plt.title('Scales distribution in the Y-axis')
 plt.xlabel('Scale value')
